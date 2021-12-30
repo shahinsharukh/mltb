0d0e6dbbe5bebe29dc09694fa1c037541962fef3
course9870 <96621049+course9870@users.noreply.github.com>
2021-12-30 19:58:28 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,19 +44,19 @@ def stats(update, context):
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
     stats = f'╭───『🤖 𝑩𝒐𝒕 𝑺𝒕𝒂𝒕𝒊𝒄𝒔 🤖』\n│\n├─⏳<b>Bot Uptime:</b> {currentTime}\n│\n'\
-            f'├─💽<b>Total Disk Space:</b> {total}\n'\
+            f'├─💽<b>Total Storage:</b> {total}\n'\
             f'├─💻<b>Used:</b> {used}\n├─💾<b>Free:</b> {free}\n'\
             f'├─📤<b>Upload:</b> {sent}\n'\
-            f'├─📥<b>Download:</b> {recv}\n'\
+            f'├─📥<b>Download:</b> {recv}\n│\n'\
             f'├─🖥️<b>CPU:</b> {cpuUsage}%\n'\
             f'├─📏<b>RAM:</b> {mem_p}%\n'\
-            f'├─💿<b>DISK:</b> {disk}%\n'\
+            f'├─💿<b>DISK:</b> {disk}%\n│\n'\
             f'├─🛰️<b>Physical Cores:</b> {p_core}\n'\
             f'├─⚙️<b>Total Cores:</b> {t_core}\n'\
-            f'├─⚡<b>SWAP:</b> {swap_t}\n├─🔱<b>Used:</b> {swap_p}%\n'\
+            f'├─⚡<b>SWAP:</b> {swap_t}\n├─🔱<b>Used:</b> {swap_p}%\n│\n'\
             f'├─💽<b>Memory Total:</b> {mem_t}\n'\
             f'├─💾<b>Memory Free:</b> {mem_a}\n'\
-            f'├─💻<b>Memory Used:</b> {mem_u}\n'\
+            f'├─💻<b>Memory Used:</b> {mem_u}\n│\n'\
             f'╰───『💥 <a href="https://t.me/+SpnPh2Gc8kHwwAAF"><b>𝐑𝐨𝐨𝐭 𝐆𝐚𝐦𝐞𝐫</b></a> 💥』\n'
     sendMessage(stats, context.bot, update)
 
@@ -241,11 +241,11 @@ def main():
     if os.path.isfile(".restartmsg"):
         with open(".restartmsg") as f:
             chat_id, msg_id = map(int, f)
-        bot.edit_message_text("╭──《♻️ 𝐁𝐎𝐓 𝐆𝐎𝐓 𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐄𝐃 ♻️》\n│\n├─🌀𝐑𝐞-𝐌𝐢𝐫𝐫𝐨𝐫 𝐘𝐨𝐮𝐫 𝐓𝐡𝐢𝐧𝐠\n│\n├─🖥️ #𝐑𝐞𝐬𝐭𝐚𝐫𝐭𝐞𝐝\n│\n╰──『💥 𝐑𝐨𝐨𝐭 𝐆𝐚𝐦𝐞𝐫 💥』", chat_id, msg_id)
+        bot.edit_message_text("╭──『♻️ 𝐁𝐎𝐓 𝐆𝐎𝐓 𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐄𝐃 ♻️』\n│\n├─🌀𝐑𝐞-𝐌𝐢𝐫𝐫𝐨𝐫 𝐘𝐨𝐮𝐫 𝐓𝐡𝐢𝐧𝐠\n│\n├─🖥️ 𝐑𝐞𝐬𝐭𝐚𝐫𝐭𝐞𝐝\n│\n╰──『💥 𝐑𝐨𝐨𝐭 𝐆𝐚𝐦𝐞𝐫 💥』", chat_id, msg_id)
         os.remove(".restartmsg")
     elif OWNER_ID:
         try:
-            text = "♻️ 𝐁𝐎𝐓 𝐆𝐎𝐓 𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐄𝐃 ♻️\n\n\n🤖𝙿𝙻𝙴𝙰𝚂𝙴 𝚁𝙴-𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙰𝙶𝙰𝙸𝙽🤖\n\n🖥️#Restarted\n\n<i><b>Root Gamer"
+            text = "╭──『♻️ 𝐁𝐎𝐓 𝐆𝐎𝐓 𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐄𝐃 ♻️』\n│\n├─🌀𝐑𝐞-𝐌𝐢𝐫𝐫𝐨𝐫 𝐘𝐨𝐮𝐫 𝐓𝐡𝐢𝐧𝐠\n│\n├─🖥️ 𝐑𝐞𝐬𝐭𝐚𝐫𝐭𝐞𝐝\n│\n╰──『💥 𝐑𝐨𝐨𝐭 𝐆𝐚𝐦𝐞𝐫 💥』"
             bot.sendMessage(chat_id=OWNER_ID, text=text, parse_mode=ParseMode.HTML)
             if AUTHORIZED_CHATS:
                 for i in AUTHORIZED_CHATS:
